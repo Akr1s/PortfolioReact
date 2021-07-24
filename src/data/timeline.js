b6de1bf6b81fc79129c18f82c7fac832120eaff6
Akr1s <dimakr71@gmail.com>
2021-07-24 11:45:48 +0300

    added real dates and events to timeline

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -4,20 +4,20 @@ const timeline = [
     description: "Discovered programming",
   },
   {
-    title: "2017",
-    description: "Discovered programming",
+    title: "2018",
+    description: "Started learning Java",
   },
   {
-    title: "2017",
-    description: "Discovered programming",
+    title: "2020, 6",
+    description: "Finished HTML/CSS course",
   },
   {
-    title: "2017",
-    description: "Discovered programming",
+    title: "2020, 9",
+    description: "Discovered React",
   },
   {
-    title: "2017",
-    description: "Discovered programming",
+    title: "2020, 9 - today",
+    description: "Have been improving my English and building projects",
   },
 ];
 
